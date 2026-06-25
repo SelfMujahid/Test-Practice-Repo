2d5cbe6fff6941551e1f26503d9e03402472c5f8
SelfMujahid <137253456+SelfMujahid@users.noreply.github.com>
2026-06-25 15:05:04 +0500

    Enhance connectAllMarkets to handle non-list responses
    Updated the connectAllMarkets method to handle non-list responses by returning an empty list when the parsed data is not a list.

--- lib/services/binance_sockets.dart
+++ lib/services/binance_sockets.dart
@@ -2,13 +2,17 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'dart:convert';
 
 class BinanceSocketsService {
-  // All market tickers fetch block
+  // Ultra-optimized global stream for all tickers
   Stream<List<dynamic>> connectAllMarkets() {
     final channel = WebSocketChannel.connect(
       Uri.parse('wss://stream.binance.com:9443/ws/!ticker@arr'),
     );
     return channel.stream.map((snapshot) {
-      return jsonDecode(snapshot) as List<dynamic>;
+      final parsed = jsonDecode(snapshot);
+      if (parsed is List) {
+        return parsed;
+      }
+      return [];
     });
   }
 }
