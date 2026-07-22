ba756f22d966d02d88daf2c83f69469f26870d98
SelfMujahid <137253456+SelfMujahid@users.noreply.github.com>
2026-07-22 11:30:23 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Simple main activity layout/code
+        setContentView(R.layout.activity_main)
     }
 }
