25414a6dfa90870bf59fe30f73a965d0d09337fe
SelfMujahid <137253456+SelfMujahid@users.noreply.github.com>
2026-07-22 13:47:35 +0500

    Update CryptoItem.kt

--- app/src/main/java/com/practice/cryptoapp/CryptoItem.kt
+++ app/src/main/java/com/practice/cryptoapp/CryptoItem.kt
@@ -1,8 +1,14 @@
 package com.practice.cryptoapp
 
+enum class PriceState {
+    UP, DOWN, NEUTRAL
+}
+
 data class CryptoItem(
     var rank: Int,
     val symbol: String,
     var price: String,
-    val marketCap: Double
+    val marketCap: Double,
+    var rawPrice: Double = 0.0,
+    var priceState: PriceState = PriceState.NEUTRAL
 )
